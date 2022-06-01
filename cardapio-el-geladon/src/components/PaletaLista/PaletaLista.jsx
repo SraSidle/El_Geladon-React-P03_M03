f692aeab176594dab975936a900588fc0d7de5d1
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-06-01 19:16:32 -0300

    resolvido o problema de nao redenrizar

--- cardapio-el-geladon/src/components/PaletaLista/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista/PaletaLista.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
-import { paletas } from "../mocks/Paletas";
+import React, { useState, useEffect } from "react";
+//import { paletas } from "../mocks/Paletas"; //retira, pois não será mais utilizado
 import "./PaletaLista.css";
 import PaletaListaItem from "../PaletaListaItem/PaletaListaItem";
+import { PaletaService } from "../../services/PaletaService";
+//useState controla o estado da aplicação, e o useEffect é responsável por controlar o ciclo de vida da aplicação
 
 function PaletaLista() {
+  const [paletas, setPaletas] = useState([]); //pq aqui é com colchetes e o de baixo é com chaves?
   const [paletaSelecionada, setPaletaSelecionada] = useState({}); // tinha um erro aqui, estava entre chaves e não entre colchetes
 
   const adicionarItem = (paletaIndex) => {
@@ -24,6 +27,15 @@ function PaletaLista() {
 
     return
   };
+
+  const getLista = async ()=> {
+      const response = await PaletaService.getLista();
+      setPaletas(response)
+  }
+
+  useEffect(()=> {
+    getLista();
+  },[]) //Esse array deve ser colocado, senão a aplicação rodará em loop infinito
   
   return (
     <div className="PaletaLista">
