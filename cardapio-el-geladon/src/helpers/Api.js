c7f177dc15064d7b6751b0ab67e48d672f71d496
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-07 22:31:44 -0300

    configuracao das rotas

--- cardapio-el-geladon/src/helpers/Api.js
+++ cardapio-el-geladon/src/helpers/Api.js
@@ -1,14 +1,20 @@
+/* Será importado no services/PaletaService.js
+Esse é um arquivo para configuração das rotas da Api, com todas as URLs de cada função
+que será executada, a partir de um arquivo jsx configurado para determinado método. (PUT, DELETE, etc.*/
+
 const PaletaContext = {
-    paletaEndpoint: () => `${Api.baseUrl}/paletas`,
-    paletaLista: () => `${PaletaContext.paletaEndpoint()}/all-paletas`,
-    paletaById: (id) => `${PaletaContext.paletaEndpoint()}/one-paleta/${id}`, //Se der erro, pode ser que precise de aspas
-    createPaleta: () => `${PaletaContext.paletaEndpoint()}/create-paleta`,
-    updatePaletaById: (id) => `${PaletaContext.paletaEndpoint()}/update-paleta/${id}`,
-    deletePaletaById: (id) => `${PaletaContext.paletaEndpoint()}/delete-paleta/${id}`
+  paletaEndpoint: () => `${Api.baseUrl}/paletas`,
+  paletaLista: () => `${PaletaContext.paletaEndpoint()}/all-paletas`,
+  paletaById: (id) => `${PaletaContext.paletaEndpoint()}/one-paleta/${id}`, //Se der erro, pode ser que precise de aspas
+  createPaleta: () => `${PaletaContext.paletaEndpoint()}/create-paleta`,
+  updatePaletaById: (id) =>
+    `${PaletaContext.paletaEndpoint()}/update-paleta/${id}`,
+  deletePaletaById: (id) =>
+    `${PaletaContext.paletaEndpoint()}/delete-paleta/${id}`,
 };
 
-//linha 3 => Essa sintaxe é pq vai pegar os valores de paletaEndpoint e redenrizar todas as paletas
-export const Api ={
-    baseUrl: "https://api-elgeladon.herokuapp.com",
-    ...PaletaContext //"..." spread operator
-}
+export const Api = {
+  baseUrl: "https://api-elgeladon.herokuapp.com",
+  ...PaletaContext, 
+  /*"..." Spread Operator, é como se "espalhasse" todos os dados de PaletaContext e colocasse dentro da const Api*/
+};
--- cardapio-el-geladon/src/services/PaletaService.js
+++ cardapio-el-geladon/src/services/PaletaService.js
@@ -1,17 +1,52 @@
-import {Api} from '../helpers/Api';
+import { Api } from "../helpers/Api";
+/*Linha 3: receberá o response(resposta da Api) e converterá em json
+  PaletaService{} => Dentro desse objeto, serão acionados os métodos do protoclo HTTP(GET, PUT, DELETE, POST), que obterá
+  funções que retornarão cada requisição do backend
+
+  Ex: getLista() => vai receber a URL da API que tem todas as paletas e o seu método para que isso seja feito,
+   "method: GET" e a conversão para json.
+ 
+  *fetch é uma api nativa do JS, que permite fazer requisições/conexões com o backend. Dentro dele é passada 
+  fetch(arquivo com as URLs das rotas importado.função que contém a URL da rota que fará algo específico(), method: "GET || POST || PUT || DELETE").Então(converte para Json)
+  fetch(Api.paletaLista(), method: "GET").then(parseResponse)
+
+  A api ela fornece apenas: _id, descricao, foto, preco e sabor. Não tem a propriedade "título".Então, para isso, 
+  será necessário pegar os dados da API e estruturar em um padrão reconhecível pelo front, para assim, não precisar modificar 
+  o paletaLista. Isso será feito por meio da função "transformPaleta".
+
+  dados fixos(mockados, as imagens pode ficar na src e serem renderizadas), mas em dados dinâmicos, como os da API, eles 
+  devem ficar dentro de assets que deverá estar dentro da public.
+*/
 
 const parseResponse = (response) => response.json();
 
+const transformPaleta = (paleta) => {
+  const [sabor, recheio] = paleta.sabor.split(" com ");
+
+  return {
+    ...paleta,
+    id: paleta._id,
+    titulo: paleta.sabor,
+    sabor,
+    ...(recheio && { recheio }),
+    possuiRecheio: Boolean(recheio),
+  };
+};
+
+const parseTransformLista = (response) => parseResponse(response).then(paletas => paletas.map(transformPaleta));
+
+const parseTransformItem = (response) => parseResponse(response).then(transformPaleta);
+
 export const PaletaService = {
-    getLista: () =>
-        fetch(Api.paletaLista(), { method: "GET"}).then(parseResponse),
-//then=então. fetch fará a requisição na API através do método "GET", "ENTÃO", após obter as respostas, converterá para json.
-    getById: (id) =>
-        fetch(Api.paletaById(id), { method: "GET"}).then(parseResponse),
+  getLista: () =>
+    fetch(Api.paletaLista(), { method: "GET" }).then(parseTransformLista),
+    getById: (id) => fetch(Api.paletaById(id), { method: "GET" }).then(parseTransformItem),
     create: () =>
-        fetch(Api.createPaleta(), {method: "POST"}).then(parseResponse),
-    updateById: (id) =>
-        fetch(Api.updatePaletaById(id), {method: "PUT"}).then(parseResponse),
-    deleteById: (id) => 
-        fetch(Api.deletePaletaById(id), {method: "DELETE"}).then(parseResponse),
-}
+    fetch(Api.createPaleta(), { method: "POST" }).then(parseResponse),
+  updateById: (id) =>
+    fetch(Api.updatePaletaById(id), { method: "PUT" }).then(parseResponse),
+  deleteById: (id) =>
+    fetch(Api.deletePaletaById(id), { method: "DELETE" }).then(parseResponse),
+};
+
+
