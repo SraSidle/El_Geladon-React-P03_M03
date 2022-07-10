f1de739eb2f049e6ad18c41587610d9f77bdd9e5
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-09 22:50:34 -0300

    configuracao para destacar o atualizar

--- cardapio-el-geladon/src/components/Navbar/Navbar.jsx
+++ cardapio-el-geladon/src/components/Navbar/Navbar.jsx
@@ -1,49 +1,70 @@
-import '../Navbar/Navbar.css';
+import "../Navbar/Navbar.css";
+import { ActionMode } from "../../constants/index";
 import sacola from "../assets/icons/sacola.svg";
 import logo from "../assets/icons/logo.svg";
-//import paleta from "../assets/icons/paleta.svg"
+//import atualizar from "../assets/icons/atualizar.svg";
 
 /* Para a funcionalidade de adicionar paleta, foi colocado "createPaleta" com parâmetro da função Navbar, o qual, será
 chamado como função no onclick da img Adiconar Paleta
+
+O updatePaleta foi add como parâmetro, sendo uma função
+
+o mode está sendo utilizado para receber o valor de ActionMode, se for igual a ATUALIZAR, adicionará a classe Paleta--ativa
+ao button, colocando-o em destaque dos demais
 */
 
-function Navbar({createPaleta}){
-    return(
-        <div className="Home__header Header">
-        <div className="row">
-            <div className="Header__logo Logo">
+function Navbar({ createPaleta, updatePaleta, mode }) {
+  return (
+    <div className="Home__header Header">
+      <div className="row">
+        <div className="Header__logo Logo">
+          <img
+            src={logo}
+            width="70px"
+            alt="Logo El Geladon"
+            className="Logo__icone"
+          />
+          <span className="Logo__titulo"> El Geladon </span>
+        </div>
+        <div className="Header__opcoes Opcoes">
+          <button
+            type="button"
+            className="Opcoes__paleta Paleta"
+            onClick={() => createPaleta()}
+          >
             <img
-                src={logo}
-                width="70px"
-                alt="Logo El Geladon"
-                className="Logo__icone"
+              className="Paleta__icone"
+              src={sacola}
+              width="40px"
+              alt="Adicionar Paleta"
             />
-            <span className="Logo__titulo"> El Geladon </span>
-            </div>
-            <div className="Header__opcoes Opcoes">
-                <button
-                    type="button"
-                    className="Opcoes__paleta Paleta"
-                    onClick={() => createPaleta()}>
-                  <img className="Paleta__icone"
-                     src={sacola} 
-                     width="40px"
-                     alt="Adicionar Paleta"
-                    
-                  />
-                </button>
-            <div className="Opcoes__sacola Sacola">
-                <img
-                src={sacola}
-                width="40px"
-                className="Sacola__icone"
-                alt="Sacola de compras"
-                />
-            </div>
-            </div>
+          </button>
+          <button
+            type="button"
+            className={`Opcoes__paleta Paleta  ${
+              mode === ActionMode.ATUALIZAR && "Paleta--ativa"
+            } `}
+            onClick={() => updatePaleta()}
+          >
+            <img
+              className="Paleta__icone"
+              src={logo}
+              width="40px"
+              alt="Editar Paleta"
+            />
+          </button>
+          <div className="Opcoes__sacola Sacola">
+            <img
+              src={sacola}
+              width="40px"
+              className="Sacola__icone"
+              alt="Sacola de compras"
+            />
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Navbar;
